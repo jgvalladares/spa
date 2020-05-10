af9de60f353b9b96191888a74d3133e4214c1a49
jgvalladares <valladaresgabriela2810@gmail.com>
2020-05-10 01:37:10 -0500

    Creando Servicios

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../servicios/heroes.service';
+
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  
+  templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
 
-  ngOnInit(): void {
+  constructor( private _heroesService:HeroesService
+                ) {
   }
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
 
 }
